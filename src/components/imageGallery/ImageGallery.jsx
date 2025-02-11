1e737cc608f0f3f238f9ab3c049e57a57fc2cdba
MoiseFlori <moiseflori@outlook.com>
2025-02-11 12:41:52 +0200

    refactoring

--- src/components/imageGallery/ImageGallery.jsx
+++ src/components/imageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ImageGalleryItem from '../imageGalleryItem/ImageGalleryItem';
 import styles from './ImageGallery.module.css';
